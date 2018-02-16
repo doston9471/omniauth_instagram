a4a2f2cd6833ed974f0221a69312e72d7e6459d4
dt <doston9471@yandex.com>
2018-02-16 23:03:44 +0600

    fix env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :instagram, "INSTAGRAM_CLIENT_ID", "INSTAGRAM_CLIENT_SECRET"
   # TODO move env.variables
   provider :instagram, 
-  		"be1528c752644e288a700bd38d35130e", 
-  		"167dd56822d34ab684cccd14e2444537", 
+  		ENV['INSTAGRAM_CLIENT_ID'],
+  		ENV['INSTAGRAM_CLIENT_SECRET'], 
   		scope: 'basic'#+public_content+follower_list+comments+relationships+likes'
 end
